7bb1e34e13bfa6d084c7c9d1dffe2cbad885a8fb
Eren <erenzaybay@gmail.com>
2020-05-18 13:17:06 +0800

    major update

--- daily_practice/1.vim_practice.sh
+++ daily_practice/1.vim_practice.sh
@@ -1,9 +1,77 @@
-# Vim Training
+
+# Knowledge:
+
+Vim is a fancy text editor with superpower.
+
+These notes are based on Venture into vim course provided by tutsplus
+
+## .vimrc introduction
+
+* .vimrc is a vim configuration file that saves your vim preference.
+
+* .vimrc file in windows is located in the user directory C:\Users\erenzaybay\.vimrc
+
+* .vimrc file in linux is located in the user folder ~\.vimrc
+
+## 4 Modes
+
+There are three mode:
+
+* i    insert mode for inserting
+
+* v    visual mode for yanking
+
+* vl   visual line mode yanking
+
+* Esc  normal mode, aka command mode for giving command
+
+## Terms
+
+* Repeater: numbers can be used as repeater.
+
+* Buffer:Buffer is a place in memory, which is showed as a list of files , you need to save the file to goto the next buffer.
+
+* Tabs and Windows: When opening tabs and windows, the tabs are one level higher in structural logic.Basically windows are nested in tabs.
+
+* .swp files: the temporarily save files that exists only when there is an abnormal exit, you can retrieve unsaved file from the file, and you need to manually remove the file when they are not useful any more.
+
+## Notation
+
+Notation used in this note ( the same is used in vim official doc file ):
+
+* dap: in sequence , press d , then a then p
+
+* <C-n>: hold down Ctrl and then n at the same time
+
+* G<C-n>: press and release G, then press Ctrl and n at the same time
+
+* placeholders: {motion} {a-zA-z} {char} {int} {repeater} {register_name}
+
+* \<Esc>  Escape key
+* \<CR>  Carriage return key or enter key
+* \<Ctrl>   Ctrl key
+* \<Tab> tab key
+* \<Shift> shift key
+* \<S-Tab> press the shift and tab key at the same time
+* \<space> space bar
+* Using space as indication of mode change: cw replacement \<Esc>
+
+## Things to remember
+write :set fdm=marker not :set fdm = marker  (no space)
+
+
+
+
+
+
+
+
+
 
 # Basic editing
 
-
-j
+# navigatin basic movement: 
+j	# move cursor up a character  
 k	# move cursor down a character
 h	# move cursor left a character
 l	# move cursor right a character
@@ -152,7 +220,11 @@ gv  # reselect the part you selected in visual mode
 :set nolist       hid the effect of set list
 
 
-#advanced
+
+
+
+
+
 
  
 # Recording Macro and calling Register
@@ -230,4 +302,6 @@ Ctrl-w r # Swap top/bottom or left/right split
 Ctrl-w o # Close every window in the current tabview but the current one
 :sb2         #open buffer 2 horrizontally
 :vert sb2    #open buffer 2 vertically
+
+
 
